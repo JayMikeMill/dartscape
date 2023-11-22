2a9f259c431211e793d0333ed011c358ee46338c
Jason Millis <jaymikemill@gmail.com>
2023-11-22 09:54:50 -0500

    auto commit from batch file

---
+++ git auto push.bat
@@ -0,0 +1,3 @@
+call git add .
+call git commit -m "auto commit from batch file"
+call git push -u origin main
--- git commit.bat
+++
@@ -1,3 +0,0 @@
-call git add .
-call git commit -m "auto commit from batch file"
-call git push -u origin main
